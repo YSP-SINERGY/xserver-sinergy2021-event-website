d380b03d02bae377bc97b99d12d5fb54b01b867c
seiya303 <seiya.u77@gmail.com>
2022-01-03 17:23:13 +0900

    git add .

--- youth_ui/src/components/youth/Vote.vue
+++ youth_ui/src/components/youth/Vote.vue
@@ -64,9 +64,9 @@
         { 
           id: 1,
           desc: '大友希野',
-          range: 'お手伝いします！私たちができる地域デザイン',
-          explanation: '私たちは宮城県を中心に活動するデザインプロジェクトチームです。</br>普段は地域貢献のために、お店や商品撮影、</br>イベントやお笑いライブなど様々な分野の撮影を行なっています。</br>ぜひNONOSTYLEをご覧ください！',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY', //動画未取得
+          range: 'お手伝いします！</br>私たちができる地域デザイン',
+          explanation: '私たちは宮城県を中心に活動するデザインプロジェクトチームです。普段は地域貢献のために、お店や商品撮影、イベントやお笑いライブなど様々な分野の撮影を行なっています。</br></br>ぜひNONOSTYLEをご覧ください！',
+          youtubeUrl: 'https://www.youtube.com/embed/Luv6YusOsls', //動画未取得>元々のurlに問題あり
           imageUrl: require("@/assets/youth/y_otomo.jpg"),
           isShow: false
         },
@@ -74,16 +74,16 @@
           id: 2, 
           desc: '遠藤友香',
           range: '目指せ脱プラ！海洋プラスチック問題について考えよう！',
-          explanation: '海洋生物がゴミ袋を誤食してしまったり、</br>プラスチックバックが有料になったりと、プラスチック問題が身近になってきました。</br>その解決のために、私が卒業論文で行った</br>農学部の視点から始めるエコ製品をご紹介します！',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY', //動画未取得
+          explanation: '海洋生物がゴミ袋を誤食してしまったり、プラスチックバックが有料になったりと、プラスチック問題が身近になってきました。</br>≈その解決のために、私が卒業論文で行った農学部の視点から始めるエコ製品をご紹介します！',
+          youtubeUrl: 'https://www.youtube.com/embed/1dUrajSNHc4', 
           imageUrl: require("@/assets/youth/y_endou.jpg"),
           isShow: false
         },
         { 
           id: 3, 
           desc: '舟橋彰馬',
-          range: 'オンラインでつながるごみひろいの輪',
-          explanation: '近年注目が高まっている海洋プラスチックごみ問題ですが、私たちにも身近な問題です。</br>生活には欠かせないプラスチックが海洋環境を危機にさらしています。</br>私たちPSWと一緒に取り組んでほしいと思っています。',
+          range: 'オンラインでつながる</br>ごみひろいの輪',
+          explanation: '近年注目が高まっている海洋プラスチックごみ問題ですが、私たちにも身近な問題です。</br></br>生活には欠かせないプラスチックが海洋環境を危機にさらしています。</br></br>私たちPSWと一緒に取り組んでほしいと思っています。',
           youtubeUrl: 'https://www.youtube.com/embed/J_WY6-6GJjM',
           imageUrl: require("@/assets/youth/y_funabashi_shouma.jpg"),
           isShow: false
@@ -92,16 +92,16 @@
           id: 4,  
           desc: '東郷泰暉',
           range: 'PEACE BIWAICHIプロジェクト',
-          explanation: 'PEACE BIWAICHIプロジェクトについて発表します。</br>みなさんに３つの問題を考えていただき、</br>そこから生まれた活動の内容を紹介しています。</br>このプロジェクトに関心を持って頂き、ぜひ一度参加して頂きたいです。',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY', //動画未取得
+          explanation: 'PEACE BIWAICHIプロジェクトについて発表します。</br></br>みなさんに３つの問題を考えていただき、</br>そこから生まれた活動の内容を紹介しています。</br></br>このプロジェクトに関心を持って頂き、ぜひ一度参加して頂きたいです。',
+          youtubeUrl: 'https://www.youtube.com/embed/f5qNV7ylNeg',
           imageUrl: require("@/assets/youth/y_tougou.jpg"),
           isShow: false
         },
         { 
           id: 5, 
           desc: '小野村華楠',
-          range: '「NOMORE Child Marriage </br>-ニジェール共和国における児童婚防止キャンペーン」',
-          explanation: '私は12歳で結婚した。</br></br>皆さんは児童婚について知っていますか？</br>児童婚は、10代の子供たちが強制的に</br>結婚をさせられることを意味します。</br></br>特にアフリカのニジェールでは、深刻な問題となっています。</br></br>そこで、今回はこの問題を解決するためのキャンペーンを立ち上げました！</br>どうぞ応援よろしくお願い致します！',
+          range: '「NOMORE Child Marriage </br>-ニジェール共和国における</br>児童婚防止キャンペーン」',
+          explanation: '私は12歳で結婚した。</br>皆さんは児童婚について知っていますか？</br>児童婚は、10代の子供たちが強制的に結婚をさせられることを意味します。</br></br>特にアフリカのニジェールでは、深刻な問題となっています。そこで、今回はこの問題を解決するためのキャンペーンを立ち上げました！どうぞ応援よろしくお願い致します！',
           youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
           imageUrl: require("@/assets/youth/y_onomura.jpg"),
           isShow: false
@@ -110,7 +110,7 @@
           id: 6, 
           desc: '五十嵐大修',
           range: 'サウナで世界平和',
-          explanation: 'いま、一大ブームが起きているサウナの魅力</br>をニートとアフリカ起業の経験がある29歳男性の観点からお話します。</br>少しでも多くの人にサウナが最高！だと伝われば嬉しいです。',
+          explanation: 'いま、一大ブームが起きているサウナの魅力をニートとアフリカ起業の経験がある29歳男性の観点からお話します。</br></br>少しでも多くの人にサウナが最高！だと伝われば嬉しいです。',
           youtubeUrl: 'https://www.youtube.com/embed/gjwjMNltoc8',
           imageUrl: require("@/assets/youth/y_igarashi.jpg"),
           isShow: false
@@ -119,6 +119,7 @@
           id: 7, 
           desc: '園部達也',
           range: 'YSP活動を通して感じたことや学んだこと',
+          explanation: '',
           youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
           imageUrl: require("@/assets/youth/y_sonobe.jpg"),
           isShow: false
@@ -126,37 +127,23 @@
         { 
           id: 8, 
           desc: '齋藤柱導',
-          range: 'アフリカの布を使ったアパレルでの、新しい国際貢献',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
+          range: 'なぜアフリカへの支援活動を辞めてオシャレをするのか',
+          explanation: '自分のことが嫌いだった私を変えてくれたのはアフリカでした。</br></br>しかし、現地人からのたった一言がきっかけで、ある悩みが生まれました。</br></br>それを解決し、愛と誇りに溢れた未来のために、私はオシャレをしています。',
+          youtubeUrl: 'ttps://www.youtube.com/embed/mefEJadgUXg',
           imageUrl: require("@/assets/youth/y_saitou.jpg"),
           isShow: false
         },
-        // { 
-        //   id: 9, 
-        //   desc: '佐々木啓徳',
-        //   range: '私の働き方',
-        //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
-        //   imageUrl: require("@/assets/youth/y_9.jpg"),
-        //   isShow: false
-        // },
-        // { 
-        //   id: 10, 
-        //   desc: '源田礼奈',
-        //   range: '「ホームステイを通して世界平和を実現する」をビジョンとして',
-        //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
-        //   imageUrl: require("@/assets/youth/y_10.jpg"),
-        //   isShow: false
-        // },
         { 
-          id: 11, 
+          id: 9, 
           desc: '熊谷有未', // 船橋光治から変更
-          range: '地球村フェス　2回から3回目に向けて',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
+          range: '「共生・共栄・共義」ここに未来が有る',
+          explanation: '世界情勢を見て、不安を感じたことはありませんか？</br></br>世界は過去に1度も平和を見たことがありません。</br>このまま進んでも未来は見えないかもしれません。</br></br>「共生・共栄・共義」が鍵になると思っています！',
+          youtubeUrl: 'https://www.youtube.com/embed/w01sRY9hgdc',//動画未取得>元々のurlに問題あり
           imageUrl: require("@/assets/youth/girl.jpg"),
           isShow: false
         },
         // { 
-        //   id: 12, 
+        //   id: 10, //連絡なし、追加可能性あり
         //   desc: '木村晃介',
         //   range: 'SDGsと科学技術',
         //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
@@ -164,7 +151,7 @@
         //   isShow: false
         // },
         // { 
-        //   id: 13, 
+        //   id: 11, //連絡なし、追加可能性あり
         //   desc: '本田裕明',
         //   range: '人口減少',
         //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
@@ -172,7 +159,7 @@
         //   isShow: false
         // },
         // { 
-        //   id: 14, 
+        //   id: 12, //連絡なし、追加可能性あり
         //   desc: '福井咲良',
         //   range: 'ソーシャルワーク',
         //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
