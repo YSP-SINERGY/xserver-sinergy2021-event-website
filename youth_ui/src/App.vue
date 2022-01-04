680592fcca607fe7bdc63b566ef0e9df5cad345f
seiya303 <seiya.u77@gmail.com>
2022-01-05 04:30:03 +0900

    completed implementation of cookie management and fetching public ip address of a browser

--- youth_ui/src/App.vue
+++ youth_ui/src/App.vue
@@ -9,6 +9,8 @@
 </template>
 
 <script>
+import VueCookies from 'vue-cookies';
+import axios from 'axios';
 import Youth from './components/Youth.vue'
 import NavigationMenu from './components/youth/NavigationMenu.vue'
 import CrowdFunding from './components/youth/CrowdFunding.vue'
@@ -21,14 +23,23 @@ export default {
   },
   data: () => ({
   }),
-  mounted() {
+  mounted() { // SEO強化のため。
     const title = "シナジー2021特設サイト-青年(Youth)";
     const description = "S!NERGY（シナジー) それは、地域社会や世界をワクワクさせたい若者たちのプレゼン・イベント。 そんな若者たちの頂点として、一番ワクワクする『アイデア』と『熱量』を持つプレゼンターにベストシナジスト賞（賞金30万円）を贈っている。 2016年初開催以来、累計観客動員数は13,000名、Youtube総再生回数は約5万回を達成。 一度きりの思い出ではなく、『ワクワク』させることに本気になった若者たちを見て下さい。 あなたの『当たり前』が変わるはずです。"
     document.title = title
     document.querySelector("meta[property='og:title']").setAttribute('content', title)
     document.querySelector("meta[name='description']").setAttribute('content', description)
     document.querySelector("meta[property='og:description']").setAttribute('content', description)
-  }
+  },
+  created() {
+    if (!(VueCookies.isKey('vote_date'))) { // 投票日に関するクッキーが存在しない場合
+      VueCookies.set('vote_date', new Date().toLocaleString({ timeZone: 'Asia/Tokyo' })); // クッキーが生成された日時を日本標準時間で保存する
+    }
+    axios.get('https://api.ipify.org?format=json')
+      .then(res => {
+        VueCookies.set('ip', res.data.ip);
+    });
+  },
 };
 </script>
 
