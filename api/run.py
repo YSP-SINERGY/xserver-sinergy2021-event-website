88ea928662366573886e77cdb70222a3e94cf9dd
seiya303 <seiya.u77@gmail.com>
2022-01-06 00:31:30 +0900

    refactored on Lambda and RDS using zappa

--- api/run.py
+++ api/run.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from views import Vote
+from views import YouthVote, TeensVote
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
 
-api.add_resource(Vote, '/api/v1/votes/') # api endpoint
+api.add_resource(YouthVote, '/api/v1/youth_votes/') # api endpoint
+api.add_resource(TeensVote, '/api/v1/teens_votes/') # api endpoint
 
 if __name__ == "__main__":
     app.run()
