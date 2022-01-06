d0a398440af966811652f6f0cd10a23b2ed2b1be
yuikateraoka <50617589+yuikateraoka@users.noreply.github.com>
2022-01-07 06:51:50 +0900

    投票システムの画像差し替え&PCサイトの文字サイズ調整 (#22)
    [マージ理由]
    １．投票システムの画像差し替え
    圧縮版の画像に差し替え、読み込み速度を上げました。
    ２．PCサイトの文字サイズ調整
    文字サイズにばらつきがあり、見辛かったので修正しました。
    [小さい]
    - What's S!NERGYの説明文
    - 青木代表の本文
    [大きい]
    - twitterの埋め込み
    
    [コミット履歴]
    * PCサイトの文字サイズ調整をしました。
    
    * 青木さんの本文の文字サイズをPCni変更しました。
    
    * twitterの埋め込みサイズを変更しました。

--- teens_ui/src/components/teens/Top.vue
+++ teens_ui/src/components/teens/Top.vue
@@ -2,12 +2,12 @@
    <div> 
     <h1 class="head-border my-10" >
       <a id="index1">
-         <p>What's S!NERGY ?</p>
+         What's S!NERGY ?
       </a>
     </h1>   
       <v-container 
         fluid 
-        class="grey lighten-4 my-15"
+        class="grey lighten-4 pa-2"
       >
         <div class = "top-youtube">
           <iframe
@@ -18,16 +18,43 @@
           />
         </div>
       </v-container>
-      <p class="sinergy-content-title text-left">
-        <strong >S!NERGY（シナジー)</strong>
+
+      <div class = "hidden-sm-and-down"> 
+      <p class="sinergy-content-title  text-left text-h5 mt-15">
+        <strong >S!NERGY（シナジー)</strong> 
       </p>
-      <p class="sinergy-content grey--text text--darken-2"><strong>
+      <p class="
+        sinergy-content 
+        grey--text 
+        text--darken-2
+        hidden-sm-and-down
+        text-subtitle-1"
+      ><strong>
         それは、地域社会や世界をワクワクさせたい若者たちのプレゼン・イベント。<br><br>
         そんな若者たちの頂点として、一番ワクワクする『アイデア』と『熱量』を持つプレゼンターにベストシナジスト賞（賞金30万円）を贈っている。<br><br>
         2016年初開催以来、累計観客動員数は13,000名、Youtube総再生回数は約5万回を達成。<br><br>
         一度きりの思い出ではなく、『ワクワク』させることに本気になった若者たちを見て下さい。<br><br>
         あなたの『当たり前』が変わるはずです。<br>
       </strong></p>
+      </div>
+
+      <div class = "hidden-sm-and-up pa-6">
+      <p class=" text-left text-subtitle-1">
+        <strong >S!NERGY（シナジー)</strong>
+      </p>
+      <p p class="
+         text-left
+         grey--text 
+         text--darken-2
+         hidden-sm-and-up
+         text-caption"><strong>
+        それは、地域社会や世界をワクワクさせたい若者たちのプレゼン・イベント。<br><br>
+        そんな若者たちの頂点として、一番ワクワクする『アイデア』と『熱量』を持つプレゼンターにベストシナジスト賞（賞金30万円）を贈っている。<br><br>
+        2016年初開催以来、累計観客動員数は13,000名、Youtube総再生回数は約5万回を達成。<br><br>
+        一度きりの思い出ではなく、『ワクワク』させることに本気になった若者たちを見て下さい。<br><br>
+        あなたの『当たり前』が変わるはずです。<br>
+      </strong></p>
+      </div>
     </div>
 </template>
 
@@ -52,14 +79,13 @@ iframe{
 
 .sinergy-content {
   &-title {
-    margin-left: 8%;
+    margin-left: 5%;
   }
-  padding: 8%;
+  padding: 5%;
   font-size: 0.7rem;
   line-height: 25px;
   text-align: justify;
   text-justify: inter-ideograph;
 }
-
 </style>
 
