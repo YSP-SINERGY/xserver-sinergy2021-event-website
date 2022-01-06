6711fd193ad81dd25b076a993e49c815a91fb277
yuika <yuikasunmoon@gmail.com>
2022-01-06 11:30:31 +0900

    youthページのプレゼンター情報の変数名を変更しました。

--- youth_ui/src/components/youth/Vote.vue
+++ youth_ui/src/components/youth/Vote.vue
@@ -43,7 +43,7 @@
             color="blue-grey darken-4"
             class="white--text"
           >
-            {{ item.desc }}
+            {{ item.presenter }}
           </v-btn>
           
         </v-col>
@@ -75,8 +75,8 @@
       this.items = [
         { 
           id: 1,
-          desc: '大友希野',
-          range: 'お手伝いします！</br>私たちができる地域デザイン',
+          presenter: '大友希野',
+          caption: 'お手伝いします！</br>私たちができる地域デザイン',
           title: 'お手伝いします！</br>私たちができる地域デザイン',
           explanation: '私たちは宮城県を中心に活動するデザインプロジェクトチームです。普段は地域貢献のために、お店や商品撮影、イベントやお笑いライブなど様々な分野の撮影を行なっています。</br></br>ぜひNONOSTYLEをご覧ください！',
           youtubeUrl: 'https://www.youtube.com/embed/Luv6YusOsls', //動画未取得>元々のurlに問題あり
@@ -85,8 +85,8 @@
         },
         { 
           id: 2, 
-          desc: '遠藤友香',
-          range: '目指せ脱プラ！海洋プラスチック問題について考えよう！',
+          presenter: '遠藤友香',
+          caption: '目指せ脱プラ！海洋プラスチック問題について考えよう！',
           title: '目指せ脱プラ！</br>海洋プラスチック問題！',
           explanation: '海洋生物がゴミ袋を誤食してしまったり、プラスチックバックが有料になったりと、プラスチック問題が身近になってきました。</br>≈その解決のために、私が卒業論文で行った農学部の視点から始めるエコ製品をご紹介します！',
           youtubeUrl: 'https://www.youtube.com/embed/1dUrajSNHc4', 
@@ -95,8 +95,8 @@
         },
         { 
           id: 3, 
-          desc: '舟橋彰馬',
-          range: 'オンラインでつながる</br>ごみひろいの輪',
+          presenter: '舟橋彰馬',
+          caption: 'オンラインでつながる</br>ごみひろいの輪',
           title: 'オンラインでつながる</br>ゴミ拾い',
           explanation: '近年注目が高まっている海洋プラスチックごみ問題ですが、私たちにも身近な問題です。</br></br>生活には欠かせないプラスチックが海洋環境を危機にさらしています。</br></br>私たちPSWと一緒に取り組んでほしいと思っています。',
           youtubeUrl: 'https://www.youtube.com/embed/J_WY6-6GJjM',
@@ -105,8 +105,8 @@
         },
         { 
           id: 4,  
-          desc: '東郷泰暉',
-          range: 'PEACE BIWAICHIプロジェクト',
+          presenter: '東郷泰暉',
+          caption: 'PEACE BIWAICHIプロジェクト',
           title: 'PEACE BIWAICHI</br>プロジェクト',
           explanation: 'PEACE BIWAICHIプロジェクトについて発表します。</br></br>みなさんに３つの問題を考えていただき、</br>そこから生まれた活動の内容を紹介しています。</br></br>このプロジェクトに関心を持って頂き、ぜひ一度参加して頂きたいです。',
           youtubeUrl: 'https://www.youtube.com/embed/f5qNV7ylNeg',
@@ -115,8 +115,8 @@
         },
         { 
           id: 5, 
-          desc: '小野村華楠',
-          range: '「NOMORE Child Marriage </br>-ニジェール共和国における</br>児童婚防止キャンペーン」',
+          presenter: '小野村華楠',
+          caption: '「NOMORE Child Marriage </br>-ニジェール共和国における</br>児童婚防止キャンペーン」',
           title: 'ニジェールの児童婚</br>防止キャンペーン',
           explanation: '私は12歳で結婚した。</br>皆さんは児童婚について知っていますか？</br>児童婚は、10代の子供たちが強制的に結婚をさせられることを意味します。</br></br>特にアフリカのニジェールでは、深刻な問題となっています。そこで、今回はこの問題を解決するためのキャンペーンを立ち上げました！どうぞ応援よろしくお願い致します！',
           youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
@@ -125,8 +125,8 @@
         },
         { 
           id: 6, 
-          desc: '五十嵐大修',
-          range: 'サウナで世界平和',
+          presenter: '五十嵐大修',
+          caption: 'サウナで世界平和',
           title: 'サウナで</br>世界平和',
           explanation: 'いま、一大ブームが起きているサウナの魅力をニートとアフリカ起業の経験がある29歳男性の観点からお話します。</br></br>少しでも多くの人にサウナが最高！だと伝われば嬉しいです。',
           youtubeUrl: 'https://www.youtube.com/embed/gjwjMNltoc8',
@@ -135,8 +135,8 @@
         },
         { 
           id: 7, 
-          desc: '園部達也',
-          range: 'YSP活動を通して感じたことや学んだこと',
+          presenter: '園部達也',
+          caption: 'YSP活動を通して感じたことや学んだこと',
           title: 'タイトル未定',
           explanation: '',
           youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
@@ -145,8 +145,8 @@
         },
         { 
           id: 8, 
-          desc: '齋藤柱導',
-          range: 'なぜアフリカへの支援活動を辞めてオシャレをするのか',
+          presenter: '齋藤柱導',
+          caption: 'なぜアフリカへの支援活動を辞めてオシャレをするのか',
           title: '支援を辞めて</br>オシャレする理由',
           explanation: '自分のことが嫌いだった私を変えてくれたのはアフリカでした。</br></br>しかし、現地人からのたった一言がきっかけで、ある悩みが生まれました。</br></br>それを解決し、愛と誇りに溢れた未来のために、私はオシャレをしています。',
           youtubeUrl: 'https://www.youtube.com/embed/mefEJadgUXg',
@@ -155,8 +155,8 @@
         },
         { 
           id: 9, 
-          desc: '熊谷有未', // 船橋光治から変更
-          range: '「共生・共栄・共義」ここに未来が有る',
+          presenter: '熊谷有未', // 船橋光治から変更
+          caption: '「共生・共栄・共義」ここに未来が有る',
           title: '「共生・共栄・共義」</br>ここに未来が有る',
           explanation: '世界情勢を見て、不安を感じたことはありませんか？</br></br>世界は過去に1度も平和を見たことがありません。</br>このまま進んでも未来は見えないかもしれません。</br></br>「共生・共栄・共義」が鍵になると思っています！',
           youtubeUrl: 'https://www.youtube.com/embed/w01sRY9hgdc',//動画未取得>元々のurlに問題あり
@@ -165,34 +165,26 @@
         },
          { 
           id: 10, 
-          desc: '本田裕明',
-          range: '長崎市の人口減少ってやばくね？</br>【未来世代まで幸せになれる長崎を目指して】',
+          presenter: '本田裕明',
+          caption: '長崎市の人口減少ってやばくね？</br>【未来世代まで幸せになれる長崎を目指して】',
           title: '未来世代まで幸せになれる</br>長崎を目指して',
-          explanation: '長崎って来たことありますか？</br> 長崎って山に囲まれ海に囲まれ、観光名所に囲まれている魅力溢れる都市。それが私の町です！ そんな私の町をもっと盛り上げていけるようなアイディアを発表します！',
-          youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
+          explanation: '長崎って来たことありますか？</br> 長崎って山に囲まれ海に囲まれ、観光名所に囲まれている魅力溢れる都市。それが私の町です！</br></br>そんな私の町をもっと盛り上げていけるようなアイディアを発表します！',
+          youtubeUrl: 'https://www.youtube.com/embed/XBPcUFKLh0o',
           imageUrl: require("@/assets/youth/y_honda.jpg"),
           isShow: false
         },
         // { 
-        //   id: 10, //連絡なし、追加可能性あり
-        //   desc: '木村晃介',
-        //   range: 'SDGsと科学技術',
+        //   id: 11, //連絡なし、追加可能性あり
+        //   presenter: '木村晃介',
+        //   caption: 'SDGsと科学技術',
         //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
         //   imageUrl: require("@/assets/youth/boy.jpg"),
         //   isShow: false
         // },
         // { 
-        //   id: 11, //連絡なし、追加可能性あり
-        //   desc: '本田裕明',
-        //   range: '人口減少',
-        //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
-        //   imageUrl: require("@/assets/youth/y_13.jpg"),
-        //   isShow: false
-        // },
-        // { 
         //   id: 12, //連絡なし、追加可能性あり
-        //   desc: '福井咲良',
-        //   range: 'ソーシャルワーク',
+        //   presenter: '福井咲良',
+        //   caption: 'ソーシャルワーク',
         //   youtubeUrl: 'https://www.youtube.com/embed/lUbQjAXRqKY',
         //   imageUrl: require("@/assets/youth/girl.jpg"),
         //   isShow: false
