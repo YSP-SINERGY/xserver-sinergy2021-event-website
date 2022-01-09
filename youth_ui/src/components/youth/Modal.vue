4934e527edbe0ad37473e56b4664043b90d6e8d3
seiya303 <seiya.u77@gmail.com>
2022-01-10 08:22:19 +0900

    環境をdevに戻しました。

--- youth_ui/src/components/youth/Modal.vue
+++ youth_ui/src/components/youth/Modal.vue
@@ -128,24 +128,8 @@
     methods: {
       async sendVote () {
         // make a PATCH request to youth vote endpoint
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         // const endpoint = `https://b73jc2zkfg.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1/teens_votes/`; // dev環境
         const endpoint = `https://was71zjlhi.execute-api.ap-northeast-1.amazonaws.com/production/api/v1/youth_votes/`; // production環境
-=======
-        const endpoint = `https://b73jc2zkfg.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1/youth_votes/`; // 本番ではproductionに切り替える
->>>>>>> reconfigured this project for dev environment using dev version of rds and lambda
-=======
-        const endpoint = `https://was71zjlhi.execute-api.ap-northeast-1.amazonaws.com/production/api/v1/youth_votes/`; // 本番ではproductionに切り替える
->>>>>>> modified for production environment
-=======
-        const endpoint = `https://b73jc2zkfg.execute-api.ap-northeast-1.amazonaws.com/dev/api/v1/youth_votes/`; // 本番ではproductionに切り替える
->>>>>>> rechanged environment from production to dev
-=======
-        const endpoint = `https://was71zjlhi.execute-api.ap-northeast-1.amazonaws.com/production/api/v1/youth_votes/`; // 本番ではproductionに切り替える
->>>>>>> switched dev environment to production one for updating presenters' votes
         try {
           let response = await axios.patch(
             endpoint,
