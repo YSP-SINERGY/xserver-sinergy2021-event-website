df7730bc5b511a0a12b6cf99e179aa6a0ab71e36
yuikateraoka <50617589+yuikateraoka@users.noreply.github.com>
2022-01-12 08:23:13 +0900

    最終結果のSP版の追加と不必要な情報の消去をしました。 (#41)
    [最終結果表示をスマートフォン版で見やすいように画像を差し替えました。]
    
    * クッキー基盤の投票機能を実装しました。 (#23)
    
    [マージ理由]
    投票をするとクッキーで一定期間投票した記録が残るように実装しました。
    
    本番では１日クッキーを残しますが、テストのために1分毎に切れるようにしてあります。
    
    [以下コミット履歴]
    
    * Merge: フロントエンドの一部バグを修正 (#19)
    [マージ理由]
    一部バグを修正しました。
    1.organizerのイメージへのパスを修正
    2.クローズタグに入っていた属性を正しく修正
    
    * modified img file path of the organizer
    
    * resolved Parsing error: end-tag-with-attributes
    
    * implemented sendvote function in modal.vue which is linked to lambda backend
    
    * changed youth and teens page url to that of dev sub-domain
    
    * added a importing line of axios
    
    * implemented voting functionality where you can vote once a day for 3 days
    
    * separated cookies in youth and teens pages.
    
    * rebased on the current main and modified some parts for the production environment
    
    * クッキー管理を完成し、IPアドレスと端末情報をサーバーで管理する仕組みを作りました。 (#27)
    
    [マージ理由]
    クッキー管理を完成し、IPアドレスと端末情報をサーバーで管理する仕組みを作りました。
    IPアドレスと端末情報はDBで管理し、不正を管理します。
    クッキーは三日間１日ごとに更新される仕組みになっています。
    
    [コミット履歴]
    * added a cookie management of ip address and user agent and modified sql scripts
    
    * modified sql scripts of init and reset
    
    * implemented ip management to check eligibility for voting
    
    * changed youth and teens pages to dev ones
    
    * implemented user agent management to control voting counts of a user
    
    * completed manage ips and user terminals and cookie management
    
    * completed development for production
    
    * reconfigured this project for dev environment using dev version of rds and lambda
    
    * modified for production environment
    
    * modified remaining merges
    
    * rechanged environment from production to dev
    
    * 投票数の多い順にプレゼンターを整列しました。
    
    * 誤って消去した部分を再度追加しました。
    
    * modified a wrong url
    
    * switched dev environment to production one for updating presenters' votes
    
    * 環境をdevに戻しました。
    
    * teensも環境をdevに戻しました。
    
    * productionのための環境に切り替えました。
    
    * 最終結果のSP版の追加と不必要な情報の消去をしました。
    
    Co-authored-by: Seiya Umemoto <50127194+seiya303@users.noreply.github.com>
    Co-authored-by: seiya303 <seiya.u77@gmail.com>

--- youth_ui/src/components/youth/FinalResults.vue
+++ youth_ui/src/components/youth/FinalResults.vue
@@ -9,14 +9,23 @@
 
 <!-- 最終結果画像        --> 
     <div class = "pa-1">
+      <!-- PC        --> 
       <img
           alt = "プレゼン シナジー2021 youth"
           src="@/assets/final_results/youth_result_min.jpg" 
           width="100%"
+          class ="hidden-sm-and-down"
       > 
+     <!-- SP        --> 
+      <img
+          alt = "プレゼン シナジー2021 youth"
+          src="@/assets/final_results/youth_result_sp_min.jpg" 
+          width="100%"
+          class ="hidden-sm-and-up "
+      >
        <!-- PC        --> 
         <p 
-          class = "voting-description 
+          class = "
           text--darken-2
           text-h4
           hidden-sm-and-down
@@ -26,7 +35,7 @@
         </p>
       <!-- SP        --> 
         <p 
-          class = "voting-description 
+          class = "
           text--darken-2
           text-h6
           hidden-sm-and-up
@@ -38,10 +47,20 @@
 
 <!-- presenter情報   ベストシナジスト    --> 
     <v-row>
-        <div>
+      <!-- PC        --> 
+        <div class ="hidden-sm-and-down">
           <img
             alt = "プレゼン シナジー2021 youth"
             src="@/assets/final_results/youth_1st_min.jpg" 
+            width="100%"
+            class = "pa-1"
+          >
+        </div>
+      <!-- SP        --> 
+        <div class ="hidden-sm-and-up">
+          <img
+            alt = "プレゼン シナジー2021 youth"
+            src="@/assets/final_results/youth_1st_sp_min.jpg" 
             width="100%"
             class = "pa-1"
           >
@@ -114,80 +133,6 @@ iframe{
 	left:0;
 	width:100%;
 	height:100%;
-}
-
-.home-about__contents {
-  margin-top: 20px;
-  justify-content: center;
- 
-  &-img {
-    text-align: center;
-  }
- 
-  &-text {
-    word-wrap: break-word;
-  }
-}
-
-.v-card__text{
-  text-align: left; 
-  width: 100%;
-}
-.v-card__title{
-  padding: auto;
-}
-.section__title-text{
-  width: 100%;
-}
-.h1.a{
-  text-align: left;
-}
-
-.zoom-1 img {
-  max-width: 90%;
-  height: auto;
-  animation: animationZoom2 10s ease-in-out infinite;
-}
-.zoom {
-  max-width: 100%;
-  height: auto;
-  overflow: hidden;
-  margin-bottom: 30px
-}
-@keyframes animationZoom2 {
-  50% { transform:scale(1.1)} 
-}
-
-.p-class {
-  padding: 8%;
-  line-height: 25px;
-  text-align: justify;
-  text-justify: inter-ideograph;
-}
-.font-size {
-  font-size: 0.7rem;
-}
-.organizer-text-box {
-  height: auto;
-  text-align: left;
-  padding: 0 3%;
-}
-.margin-height-auto {
-  margin: auto 0;
-}
-@media only screen and (max-width: 600px) {
-  .font-size {
-    font-size: 0.6rem;
-  }  
-  .organizer-text-box {
-    font-size: .75rem;
-  }
-  .mobile-organizer-text-box {
-    padding: 0 1%;
-  }
-  .column-reserve-row {
-    flex-direction: column-reverse;
-  }
 }
 
 </style>
